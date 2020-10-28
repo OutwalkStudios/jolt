fa7a553da184efc53906ec69010bb0188594cbd9
Ozgun Bal <ozgunbal@gmail.com>
2020-10-28 16:38:02 +0300

    Add code-splitting for toolchains

--- packages/toolchain-javascript/rollup.config.js
+++ packages/toolchain-javascript/rollup.config.js
@@ -7,34 +7,50 @@ import { terser } from "rollup-plugin-terser";
 import { dependencies } from "./package.json";
 import { builtinModules } from "module";
 
-/* build config */
-export default {
-    input: "src/index.js",
-    output: { file: "dist/toolchain.js", format: "cjs" },
-    plugins: [
-        babel({
-            babelHelpers: "bundled",
-            exclude: "node_modules/**",
-            presets: [
-                [
-                    "@babel/preset-env",
-                    {
-                        "targets": {
-                            "node": "10.6"
-                        }
+const plugins = [
+    babel({
+        babelHelpers: "bundled",
+        exclude: "node_modules/**",
+        presets: [
+            [
+                "@babel/preset-env",
+                {
+                    "targets": {
+                        "node": "10.6"
                     }
-                ]
-            ],
-            plugins: ["@babel/plugin-proposal-class-properties"]
-        }),
-        resolve(),
-        commonjs(),
-        json(),
-        terser({
-            output: {
-                preamble: "/* Copyright (c) 2020 Outwalk Studios */"
-            }
-        })
-    ],
-    external: Object.keys(dependencies).concat(builtinModules)
-}
+                }
+            ]
+        ],
+        plugins: ["@babel/plugin-proposal-class-properties"]
+    }),
+    resolve(),
+    commonjs(),
+    json(),
+    terser({
+        output: {
+            preamble: "/* Copyright (c) 2020 Outwalk Studios */"
+        }
+    })
+];
+const external = Object.keys(dependencies).concat(builtinModules)
+
+/* build config */
+export default [
+    {
+        input: "src/index.js",
+        output: { file: "dist/toolchain.js", format: "cjs" },
+        plugins,
+        external, 
+    },
+    {
+        input: [
+            "src/cmd/build.js",
+            "src/cmd/watch.js",
+            "src/cmd/lint.js",
+            "src/cmd/serve.js"
+        ],
+        output: { dir: "dist/cmd", format: "cjs", exports: "default" },
+        plugins,
+        external,
+    }
+]
